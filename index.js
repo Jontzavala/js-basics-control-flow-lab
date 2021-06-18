73cb3aa41d3a3e31270032da02ed76772e4e5af6
Jonathan Zavala <jontzavala@gmail.com>
2021-06-18 13:09:23 -0500

    commit tree 46512670fa9bc040837c4ff2e3506380297d2d1b

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function scuberGreetingForFeet(feet){
-  if (feet > 400) {
+  if (feet <= 400) {
     return "This one is on me!"
   }
   else if (feet >= 2000 && feet <=2500) {
     return "I will gladly take your thirty bucks."
   }
-  else (feet > 2500) {
+  else if (feet > 2500) {
     return "No can do."
   }
 }
