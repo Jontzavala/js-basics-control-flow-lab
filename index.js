8dc799b04fe0c3b39a073f85bdbbaf8882d81187
Jonathan Zavala <jontzavala@gmail.com>
2021-06-18 13:06:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet > 400) {
+    return "This one is on me!"
+  }
+  else if (feet >= 2000 && feet <=2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else (feet > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const zoom = city === "NYC" ? ("Ok, sounds good.") : ("No go.");
+  return zoom;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(response){
+  let responseType;
+  switch (response) {
+    case 'generous':
+      responseType = "Thank you so much."
+      break;
+    case 'not as generous':
+      responseType = "Thank you."
+      break;
+    default:
+      responseType = "Bye."
+      break;
+  }
+  return responseType
 }
